8ca5bebd7ede240a69a7a9e7eceaf8d2665d77f3
RF0510 <rosevelt.foushee@yahoo.com>
2024-07-02 14:19:06 -0400

    Exercise 2

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 /*
-Exercise 2: Add strings to the array
+Exercise 3: Insert at the beginning
 
-1) Add 'pizza' and 'cheeseburger' to the `foods` array. 
+1) Insert the string 'taco' at the beginning of the `foods` array.
 
-Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
-
-Complete Exercise 2 in the space below:
+Complete Exercise 3 in the space below:
 */
-const foods = ["pizza", "cheeseburger"]
+const foods = ["pizza", "cheeseburger"];
+foods.unshift("taco");
 
 
-console.log('Exercise 2 result:', foods);
+console.log('Exercise 3 result:', foods);
