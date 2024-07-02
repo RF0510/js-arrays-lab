6c6a3cc8d43123bffb5cf1e3f54b0dabbe5baa2f
RF0510 <rosevelt.foushee@yahoo.com>
2024-07-02 15:28:54 -0400

    Exercise 5

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 /*
-Exercise 4: Access an array element
+Exercise 5: Insert an element between two others
 
-1) Retrieve the 'pizza' string from the array based on its position (index) in
-   the array.  
+1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
 
-2) Assign it to a variable called `favFood`.
-
-Complete Exercise 4 in the space below:
+Complete Exercise 5 in the space below:
 */
 
-const foods = ["pizza", "cheeseburger"];
-const favFood = foods[0]
+const foods = ["Pizza", "cheesebuger" ];
+foods.splice(1, 0, "tofu")
 
-console.log('Exercise 4 result:', favFood);
-
+console.log('Exercise 5 result:', foods);
