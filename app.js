63886cb7b7b06d142f4ed417663374fa9744c9df
RF0510 <rosevelt.foushee@yahoo.com>
2024-07-03 23:13:02 -0400

    Exercise 13

--- app.js
+++ app.js
@@ -1,41 +1,22 @@
-/*Exercise 12: FizzBuzz with arrays
+/*
+Exercise 13: Retrieve the Last Array
 
-1) Choose a method to iterate through the `nums` array. 
+1) Assign the last nested array in the `numArrays` below to a variable named
+   `numList`. As you do this, also fulfill these goals:
 
-2. As you loop, sort the numbers into new arrays based on the following rules:
+   - Assume you don't know how many nested arrays `numArrays` contains.
+   - Do not alter the original `numArrays` array.
 
-   - Push any number evenly divisible by 3 to an array called `fizz`.
-   - Push any number evenly divisible by 5 to an array called `buzz`.
-   - Push any number that is evenly divisible by 3 and 5 to an array called
-     `fizzbuzz`.
-
-   Note: A single number may meet more than one of the above rules. If it does,
-         it should be placed in multiple arrays. For example, the number `15`
-         will appear in the `fizz`, `buzz`, and `fizzbuzz` arrays.
-
-Complete Exercise 12 in the space below:
+Complete Exercise 13 in the space below:
 */
+//used ChatGPT
+const numArrays = [
+	[100, 5, 23],
+	[15, 21, 72, 9],
+	[45, 66],
+	[7, 81, 90]
+];
 
+const numList = numArrays[numArrays.length -1];
 
-const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
-
-let fizz = [];
-let buzz = [];
-let fizzbuzz = [];
-
-for (let i = 0; i < nums.length; i++) {
-    let number = nums[i];
-
-    if (number % 3 === 0 && number % 5 === 0) {
-        fizzbuzz.push(number); 
-    } else if (number % 3 === 0) {
-        fizz.push(number); 
-    } else if (number % 5 === 0) {
-        buzz.push(number); 
-    }
-}
-
-console.log("Exercise 12 Results:");
-console.log("Fizz numbers:", fizz);
-console.log("Buzz numbers:", buzz);
-console.log("FizzBuzz numbers:", fizzbuzz);
+console.log('Exercise 13 result:', numList);
